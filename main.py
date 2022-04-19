641571cb9f6482a27f1710591a58404cc18e19df
Paranjaysaxena <paranjayasaxena@gmail.com>
2022-04-19 14:20:06 +0530

    add input

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import json
 from worker import Worker
 from function import Function
 from package import Package
 from paSch import PaSch
 
+
 def main():
     # list of workers
 
     workers = [{"worker_id": "1", "threshold": 100},
-              {"worker_id": "2", "threshold": 50},
-              {"worker_id": "3", "threshold": 150}]
+               {"worker_id": "2", "threshold": 50},
+               {"worker_id": "3", "threshold": 150}]
 
     global_workers = []
     global_hashworkers = []
@@ -21,19 +23,8 @@ def main():
 
     # list of fn
 
-    fn = [{
-        "function_id": "foo",
-        "function_imports": ["p0", "p1"],
-        "function_size": 10
-    }, {
-        "function_id": "bar",
-        "function_imports": ["p0", "p2"],
-        "function_size": 15
-    }, {
-        "function_id": "par",
-        "function_imports": ["p1", "p2"],
-        "function_size": 20
-    }]
+    func_file = open('registry/functions.json')
+    fn = json.load(func_file)
 
     global_fn = []
     for x in fn:
@@ -42,9 +33,8 @@ def main():
 
     # list of packages
 
-    pkgs = [{"package_id": "p0", "package_size": 3},
-            {"package_id": "p1", "package_size": 4},
-            {"package_id": "p2", "package_size": 5}]
+    pkgs_file = open('registry/packages.json')
+    pkgs = json.load(pkgs_file)
 
     global_pkgs = []
     for x in pkgs:
@@ -69,9 +59,11 @@ def main():
         print("2. Update an existing worker's threshold.")
         print("3. Remove a worker.")
         print("4. View all workers.")
-        print("5. Execute a function")
-        print("6. Current state of system")
-        print("7. Cache hits and miss")
+        print("5. View all functions in the registry.")
+        print("6. View all packages in the registry.")
+        print("7. Execute a function")
+        print("8. Current state of system")
+        print("9. Cache hits and miss")
         print("0. Exit")
         print("\n")
         option = int(input("Choose an option: "))
@@ -86,30 +78,34 @@ def main():
                 w_id = input("Enter worker id: ")
                 thres = int(input("Enter worker's threshold: "))
                 for i, worker in enumerate(workers):
-                    if w_id  == worker["worker_id"]:
+                    if w_id == worker["worker_id"]:
                         worker["threshold"] = thres
                         break
                 print("Worker {} successfully updated!".format(w_id))
             case 3:
                 w_id = input("Enter worker id: ")
                 for i, worker in enumerate(workers):
-                    if w_id  == worker["worker_id"]:
+                    if w_id == worker["worker_id"]:
                         del workers[i]
                 print("Worker {} successfully removed!".format(w_id))
             case 4:
-                print("Workers: ",workers)
+                print("Workers: ", workers)
             case 5:
+                print("Functions: ", fn)
+            case 6:
+                print("Packages: ", pkgs)
+            case 7:
                 f_id = input("Enter the function id: ")
                 t_stamp = int(input("Enter the timestamp: "))
                 print(scheduler.assignWorker(f_id, t_stamp))
                 print("Function {} successfully executed!".format(f_id))
-            case 6:
+            case 8:
                 t_stamp = int(input("Enter the timestamp: "))
                 scheduler.getWorkerDetails(t_stamp)
-            case 7: 
+            case 9:
                 details = scheduler.getCacheHitAndMissDetails()
                 print(details)
-            case 0: 
+            case 0:
                 exit()
             case default:
                 continue
@@ -130,8 +126,8 @@ def main():
     # send requests from here
     # DO : for x in fn_inst
 
-    # for getWorker Details create an option in while loop of inctructions to run it at a timestamp 
-    #scheduler.getWorkerDetails(timestamp=9)
+    # for getWorker Details create an option in while loop of inctructions to run it at a timestamp
+    # scheduler.getWorkerDetails(timestamp=9)
 
 
 if __name__ == "__main__":
